1b77dafdfe379db26526389115240a6f0bc1d760
Ollie Terrance <ollie.terrance@live.co.uk>
2017-04-25 23:07:15 +0100

    SlackRTM: Commands for one-bridge-per-sync
    Warning: HO->Slack isn't yet cancelled when removing a bridge.

--- hangupsbot/plugins/_chatbridge/chatbridge_slackrtm/commands.py
+++ hangupsbot/plugins/_chatbridge/chatbridge_slackrtm/commands.py
@@ -3,21 +3,11 @@ from functools import wraps
 import logging
 import re
 
-from .core import HANGOUTS, SLACK, inv
+from .core import HANGOUTS, SLACK, inv, Base
 from .parser import from_hangups
 
 
 logger = logging.getLogger(__name__)
-
-
-bridge = None
-
-def set_bridge(br):
-    """
-    Obtain a reference to the bridge instance for use with commands.
-    """
-    global bridge
-    bridge = br
 
 
 def _resolve_channel(team, query):
@@ -25,11 +15,11 @@ def _resolve_channel(team, query):
     match = re.match(r"<#(.*?)\|.*?>", query)
     if match:
         query = match.group(1)
-    if query in bridge.slacks[team].channels:
-        return bridge.slacks[team].channels[query]
+    if query in Base.slacks[team].channels:
+        return Base.slacks[team].channels[query]
     if query.startswith("#"):
         query = query[1:]
-    for channel in bridge.slacks[team].channels.values():
+    for channel in Base.slacks[team].channels.values():
         if channel["name"] == query:
             return channel
     raise KeyError(query)
@@ -40,17 +30,18 @@ def identify(source, sender, team, query=None, clear=False):
     Create one side of an identity link, either Hangouts->Slack or Slack->Hangouts.
     """
     dest = inv(source)
-    idents = bridge.idents[team]
+    idents = Base.idents[team]
+    slack = Base.slacks[team]
     if query:
         if source == HANGOUTS:
-            for user_id, user in bridge.users[team].items():
+            for user_id, user in slack.users[team].items():
                 if query == user["id"] or query.lower() == user["name"]:
                     user_name = user["name"]
                     break
             else:
                 return "No user in <b>{}</b> called <b>{}</b>.".format(team, query)
         else:
-            user = bridge.bot.get_hangups_user(query)
+            user = Base.bot.get_hangups_user(query)
             if not user.definitionsource:
                 return "No user in Hangouts with ID <b>{}</b>.".format(query)
             user_id = user.id_.chat_id
@@ -74,6 +65,8 @@ def identify(source, sender, team, query=None, clear=False):
 
 
 def members(source, room):
+    # XXX: This would be better suited as a chatbridge-wide feature -- only channels connected
+    # directly to the source channel will be enumerated.
     if source == SLACK:
         team, channel = room
         try:
@@ -81,24 +74,25 @@ def members(source, room):
         except KeyError:
             return "No such channel <b>{}</b>.".format(room[1])
     else:
-        hangout = bridge.bot.get_hangups_conversation(room)
+        hangout = Base.bot.get_hangups_conversation(room)
     channels = set()
     hangouts = set()
-    for sync in bridge.configuration["syncs"]:
-        if ((source == SLACK and sync["channel"] == [team, channel["id"]]) or
-                (source == HANGOUTS and sync["hangout"] == room)):
-            channels.add(tuple(sync["channel"]))
-            hangouts.add(sync["hangout"])
+    for team, bridges in Base.bridges.items():
+        for bridge in bridges:
+            if ((source == SLACK and bridge.team == team and bridge.channel == channel["id"]) or
+                    (source == HANGOUTS and bridge.hangout == room)):
+                channels.add((bridge.team, bridge.channel))
+                hangouts.add(bridge.hangout)
     if not channels and not hangouts:
         return "This channel/hangout isn't currently being synced."
     lines = []
     for team, channel in channels:
-        channel = bridge.slacks[team].channels[channel]
+        channel = Base.slacks[team].channels[channel]
         lines.append("<b>#{}</b> ({} Slack):".format(channel["name"], team))
         for member in channel["members"]:
-            lines.append(bridge.slacks[team].users[member]["name"])
+            lines.append(Base.slacks[team].users[member]["name"])
     for hangout in hangouts:
-        hangout = bridge.bot.get_hangups_conversation(hangout)
+        hangout = Base.bot.get_hangups_conversation(hangout)
         lines.append("<b>{}</b> (Hangouts):".format(hangout._conversation.name))
         for member in hangout.users:
             lines.append(member.full_name)
@@ -112,11 +106,20 @@ def sync(team, channel, hangout):
         channel = _resolve_channel(team, channel)
     except KeyError:
         return "No such channel <b>{}</b> on <b>{}</b>.".format(channel, team)
-    for sync in bridge.configuration["syncs"]:
-        if sync["channel"] == [team, channel["id"]] and sync["hangout"] == hangout:
-            return "This channel/hangout pair is already being synced."
-    bridge.configuration["syncs"].append({"channel": [team, channel["id"]], "hangout": hangout})
-    bridge.bot.config.set_by_path(["slackrtm", "syncs"], bridge.configuration["syncs"])
+    # Make sure this team/channel/hangout combination isn't already configured.
+    for team, bridges in Base.bridges.items():
+        for bridge in bridges:
+            if bridge.team == team and bridge.channel == channel["id"] and bridge.hangout == hangout:
+                return "This channel/hangout pair is already being synced."
+    # Create a new bridge, and register it with the Slack connection.
+    # XXX: Circular dependency on bridge.BridgeInstance, commands.run_slack_command.
+    from .bridge import BridgeInstance
+    sync = {"channel": [team, channel["id"]], "hangout": hangout}
+    Base.add_bridge(BridgeInstance(Base.bot, "slackrtm", sync))
+    # Add the new sync to the config list.
+    syncs = Base.bot.config.get_by_path(["slackrtm", "syncs"])
+    syncs.append(sync)
+    Base.bot.config.set_by_path(["slackrtm", "syncs"], syncs)
     return "Now syncing <b>#{}</b> on <b>{}</b> to hangout <b>{}</b>.".format(channel["name"], team, hangout)
 
 def unsync(team, channel, hangout):
@@ -127,11 +130,18 @@ def unsync(team, channel, hangout):
         channel = _resolve_channel(team, channel)
     except KeyError:
         return "No such channel <b>{}</b> on <b>{}</b>.".format(channel, team)
-    for sync in bridge.configuration["syncs"]:
-        if sync["channel"] == [team, channel["id"]] and sync["hangout"] == hangout:
-            bridge.configuration["syncs"].remove(sync)
-            bridge.bot.config.set_by_path(["slackrtm", "syncs"], bridge.configuration["syncs"])
-            return "No longer syncing <b>#{}</b> on <b>{}</b> to hangout <b>{}</b>.".format(channel["name"], team, hangout)
+    # Make sure this team/channel/hangout combination isn't already configured.
+    for team, bridges in Base.bridges.items():
+        for bridge in bridges:
+            if bridge.team == team and bridge.channel == channel["id"] and bridge.hangout == hangout:
+                # Destroy the bridge and its event callback.
+                Base.remove_bridge(bridge)
+                # Remove the sync from the config list.
+                syncs = Base.bot.config.get_by_path(["slackrtm", "syncs"])
+                syncs.remove(bridge.sync)
+                Base.bot.config.set_by_path(["slackrtm", "syncs"], syncs)
+                return ("No longer syncing <b>#{}</b> on <b>{}</b> to hangout <b>{}</b>."
+                        .format(channel["name"], team, hangout))
     return "This channel/hangout pair isn't currently being synced."
 
 
@@ -187,7 +197,7 @@ def slack_members(bot, event, *args):
 def slack_sync(bot, event, *args):
     ("""Link a Slack channel to a hangout.\nUsage: <b>slack_sync <i>team</i> <i>channel</i> to <i>hangout</i></b>, """
      """or just <b>slack_sync <i>team</i> <i>channel</i></b> for the current hangout.""")
-    if not len(args) == 2 or len(args) == 4 and args[2] == "to":
+    if not (len(args) == 2 or len(args) == 4 and args[2] == "to"):
         return ("Usage: <b>slack_sync <i>team channel</i> to <i>hangout</i></b>, "
                 "or just <b>slack_sync <i>team channel</i></b> for the current hangout.")
     return sync(args[0], args[1], event.conv.id_ if len(args) == 2 else args[3])
@@ -196,8 +206,9 @@ def slack_sync(bot, event, *args):
 def slack_unsync(bot, event, *args):
     ("""Unlink a Slack channel from a hangout.\nUsage: <b>slack_unsync <i>team</i> <i>channel</i> from <i>hangout</i></b>, """
      """or just <b>slack_unsync <i>team</i> <i>channel</i></b> for the current hangout.""")
-    if not len(args) == 2 or len(args) == 4 and args[2] == "from":
-        return "Usage: *slack_sync _team_ _channel_ to _hangout_*, or just *slack_sync _team_ _channel_* for the current hangout."
+    if not (len(args) == 2 or len(args) == 4 and args[2] == "from"):
+        return ("Usage: <b>slack_unsync <i>team channel</i> from <i>hangout</i></b>, "
+                "or just <b>slack_unsync <i>team channel</i></b> for the current hangout.")
     return unsync(args[0], args[1], event.conv.id_ if len(args) == 2 else args[3])
 
 
@@ -208,6 +219,10 @@ def run_slack_command(msg, slack, team):
         name = args.pop(0)
     except IndexError:
         return
+    try:
+        admins = Base.bot.config.get_by_path(["slackrtm", "teams", team, "admins"])
+    except (KeyError, TypeError):
+        admins = []
     if name == "identify":
         if not len(args) or (args[0].lower(), len(args)) not in [("as", 2), ("clear", 1)]:
             return "Usage: <b>identify as <i>user</i></b> to link, <b>identify clear</b> to unlink"
@@ -220,7 +235,7 @@ def run_slack_command(msg, slack, team):
         if not len(args) == 1:
             return "Usage: <b>members <i>channel</i></b>"
         return members(SLACK, [team, args[0]])
-    elif msg.user in bridge.configuration["teams"][team]["admins"]:
+    elif msg.user in admins:
         if name == "sync":
             if not (len(args) == 3 and args[1] == "to"):
                 return "Usage: <b>sync <i>channel</i> to <i>hangout</i></b>"
